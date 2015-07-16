nikamigaming-create/nikami-openmw-lab

commit 7cdbd4e21dc4b0b476cf91c10f23a0440df90f4c

Move attackingOrSpell flag to the CharacterController

apps/openmw/mwmechanics/character.cpp
@@ -654,6 +654,7 @@ CharacterController::CharacterController(const MWWorld::Ptr &ptr, MWRender::Anim
     , mSecondsOfSwimming(0)
     , mSecondsOfRunning(0)
     , mTurnAnimationThreshold(0)
+    , mAttackingOrSpell(false)
 {
     if(!mAnimation)
         return;
@@ -937,7 +938,7 @@ bool CharacterController::updateCreatureState()
             mAnimation->disable(mCurrentWeapon);
     }
 
-    if(stats.getAttackingOrSpell())
+    if(mAttackingOrSpell)
     {
         if(mUpperBodyState == UpperCharState_Nothing && mHitState == CharState_None)
         {
@@ -997,7 +998,7 @@ bool CharacterController::updateCreatureState()
             }
         }
 
-        stats.setAttackingOrSpell(false);
+        mAttackingOrSpell = false;
     }
 
     bool animPlaying = mAnimation->getInfo(mCurrentWeapon);
@@ -1142,7 +1143,7 @@ bool CharacterController::updateWeaponState()
 
     float complete;
     bool animPlaying;
-    if(stats.getAttackingOrSpell())
+    if(mAttackingOrSpell)
     {
         if(mUpperBodyState == UpperCharState_WeapEquiped && mHitState == CharState_None)
         {
@@ -1152,7 +1153,7 @@ bool CharacterController::updateWeaponState()
             {
                 // Unset casting flag, otherwise pressing the mouse button down would
                 // continue casting every frame if there is no animation
-                stats.setAttackingOrSpell(false);
+                mAttackingOrSpell = false;
 
                 const MWWorld::ESMStore &store = MWBase::Environment::get().getWorld()->getStore();
 
@@ -2039,6 +2040,11 @@ bool CharacterController::isReadyToBlock() const
 bool CharacterController::isKnockedOut() const
 {
     return mHitState == CharState_KnockOut;
+}
+
+void CharacterController::setAttackingOrSpell(bool attackingOrSpell)
+{
+    mAttackingOrSpell = attackingOrSpell;
 }
 
 void CharacterController::setActive(bool active)

apps/openmw/mwinput/inputmanagerimp.cpp
@@ -207,7 +207,7 @@ namespace MWInput
         if (mControlSwitch["playercontrols"])
         {
             if (action == A_Use)
-                mPlayer->getPlayer().getClass().getCreatureStats(mPlayer->getPlayer()).setAttackingOrSpell(currentValue != 0);
+                mPlayer->setAttackingOrSpell(currentValue != 0);
             else if (action == A_Jump)
                 mAttemptJump = (currentValue == 1.0 && previousValue == 0.0);
         }

apps/openmw/mwmechanics/actors.cpp
@@ -1086,6 +1086,9 @@ namespace MWMechanics
 
                 iter->second->getCharacterController()->setActive(inProcessingRange);
 
+                if (iter->first == player)
+                    iter->second->getCharacterController()->setAttackingOrSpell(MWBase::Environment::get().getWorld()->getPlayer().getAttackingOrSpell());
+
                 if (!iter->first.getClass().getCreatureStats(iter->first).isDead())
                 {
                     updateActor(iter->first, duration);

apps/openmw/mwmechanics/aicombat.cpp
@@ -269,8 +269,7 @@ namespace MWMechanics
             attack = false;
         }
 
-        actorClass.getCreatureStats(actor).setAttackingOrSpell(attack);
-
+        characterController.setAttackingOrSpell(attack);
 
         float& actionCooldown = storage.mActionCooldown;
         actionCooldown -= duration;
@@ -476,7 +475,7 @@ namespace MWMechanics
             movement.mPosition[1] = 0;
             movement.mPosition[2] = 0;
             readyToAttack = false;
-            actorClass.getCreatureStats(actor).setAttackingOrSpell(false);
+            characterController.setAttackingOrSpell(false);
             return false;
         }
 

apps/openmw/mwmechanics/character.hpp
@@ -180,6 +180,9 @@ class CharacterController : public MWRender::Animation::TextKeyListener
     float mTurnAnimationThreshold; // how long to continue playing turning animation after actor stopped turning
 
     std::string mAttackType; // slash, chop or thrust
+
+    bool mAttackingOrSpell;
+
     void determineAttackType();
 
     void refreshCurrentAnims(CharacterState idle, CharacterState movement, bool force=false);
@@ -234,6 +237,8 @@ public:
     
     bool isReadyToBlock() const;
     bool isKnockedOut() const;
+
+    void setAttackingOrSpell(bool attackingOrSpell);
 
     /// @see Animation::setActive
     void setActive(bool active);

apps/openmw/mwmechanics/creaturestats.cpp
@@ -16,7 +16,7 @@ namespace MWMechanics
 
     CreatureStats::CreatureStats()
         : mDrawState (DrawState_Nothing), mDead (false), mDied (false), mMurdered(false), mFriendlyHits (0),
-          mTalkedTo (false), mAlarmed (false), mAttacked (false), mAttackingOrSpell(false),
+          mTalkedTo (false), mAlarmed (false), mAttacked (false),
           mKnockdown(false), mKnockdownOneFrame(false), mKnockdownOverOneFrame(false),
           mHitRecovery(false), mBlock(false), mMovementFlags(0),
           mFallHeight(0), mRecalcMagicka(false), mLastRestock(0,0), mGoldPool(0), mActorId(-1),
@@ -86,11 +86,6 @@ namespace MWMechanics
     const MagicEffects &CreatureStats::getMagicEffects() const
     {
         return mMagicEffects;
-    }
-
-    bool CreatureStats::getAttackingOrSpell() const
-    {
-        return mAttackingOrSpell;
     }
 
     int CreatureStats::getLevel() const
@@ -210,11 +205,6 @@ namespace MWMechanics
             mRecalcMagicka = true;
 
         mMagicEffects.setModifiers(effects);
-    }
-
-    void CreatureStats::setAttackingOrSpell(bool attackingOrSpell)
-    {
-        mAttackingOrSpell = attackingOrSpell;
     }
 
     void CreatureStats::setAiSetting (AiSetting index, Stat<int> value)
@@ -487,7 +477,6 @@ namespace MWMechanics
         state.mTalkedTo = mTalkedTo;
         state.mAlarmed = mAlarmed;
         state.mAttacked = mAttacked;
-        state.mAttackingOrSpell = mAttackingOrSpell;
         // TODO: rewrite. does this really need 3 separate bools?
         state.mKnockdown = mKnockdown;
         state.mKnockdownOneFrame = mKnockdownOneFrame;
@@ -534,7 +523,6 @@ namespace MWMechanics
         mTalkedTo = state.mTalkedTo;
         mAlarmed = state.mAlarmed;
         mAttacked = state.mAttacked;
-        mAttackingOrSpell = state.mAttackingOrSpell;
         // TODO: rewrite. does this really need 3 separate bools?
         mKnockdown = state.mKnockdown;
         mKnockdownOneFrame = state.mKnockdownOneFrame;

apps/openmw/mwmechanics/creaturestats.hpp
@@ -40,7 +40,6 @@ namespace MWMechanics
         bool mTalkedTo;
         bool mAlarmed;
         bool mAttacked;
-        bool mAttackingOrSpell;
         bool mKnockdown;
         bool mKnockdownOneFrame;
         bool mKnockdownOverOneFrame;

apps/openmw/mwworld/player.cpp
@@ -36,7 +36,8 @@ namespace MWWorld
         mForwardBackward(0),
         mTeleported(false),
         mCurrentCrimeId(-1),
-        mPaidCrimeId(-1)
+        mPaidCrimeId(-1),
+        mAttackingOrSpell(false)
     {
         ESM::CellRef cellRef;
         cellRef.blank();
@@ -214,6 +215,16 @@ namespace MWWorld
     void Player::setTeleported(bool teleported)
     {
         mTeleported = teleported;
+    }
+
+    void Player::setAttackingOrSpell(bool attackingOrSpell)
+    {
+        mAttackingOrSpell = attackingOrSpell;
+    }
+
+    bool Player::getAttackingOrSpell() const
+    {
+        return mAttackingOrSpell;
     }
 
     bool Player::isInCombat() {

commit f8a3a4a01a9642d284c42abdd296723be524dd19

Fix enchantments casting more than once per button press, broken by 7cdbd4e21dc4b0b

apps/openmw/mwmechanics/character.cpp
@@ -43,6 +43,7 @@
 #include "../mwworld/class.hpp"
 #include "../mwworld/inventorystore.hpp"
 #include "../mwworld/esmstore.hpp"
+#include "../mwworld/player.hpp"
 
 namespace
 {
@@ -1169,6 +1170,10 @@ bool CharacterController::updateWeaponState()
                 // Unset casting flag, otherwise pressing the mouse button down would
                 // continue casting every frame if there is no animation
                 mAttackingOrSpell = false;
+                if (mPtr == MWBase::Environment::get().getWorld()->getPlayerPtr())
+                {
+                    MWBase::Environment::get().getWorld()->getPlayer().setAttackingOrSpell(false);
+                }
 
                 const MWWorld::ESMStore &store = MWBase::Environment::get().getWorld()->getStore();
 