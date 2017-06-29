nikamigaming-create/nikami-openmw-lab

commit 2a0c865a2055fcc5f77cd93348d2a1c115e80ff1

Preload player cell as soon as the player is read from the savegame
Giving the worker thread something to do while the rest of the savegame is parsed.

apps/openmw/mwworld/worldimp.cpp
@@ -374,10 +374,13 @@ namespace MWWorld
                 reader.getHNT(mTeleportEnabled, "TELE");
                 reader.getHNT(mLevitationEnabled, "LEVT");
                 return;
+            case ESM::REC_PLAY:
+                mPlayer->readRecord(reader, type);
+                mWorldScene->preloadCell(getPlayerPtr().getCell(), true);
+                break;
             default:
                 if (!mStore.readRecord (reader, type) &&
                     !mGlobalVariables.readRecord (reader, type) &&
-                    !mPlayer->readRecord (reader, type) &&
                     !mWeatherManager->readRecord (reader, type) &&
                     !mCells.readRecord (reader, type, contentFileMap)
                      && !mProjectileManager->readRecord (reader, type)

apps/openmw/mwworld/scene.hpp
@@ -82,13 +82,13 @@ namespace MWWorld
             void preloadExteriorGrid(const osg::Vec3f& playerPos, const osg::Vec3f& predictedPos);
             void preloadFastTravelDestinations(const osg::Vec3f& playerPos, const osg::Vec3f& predictedPos);
 
-            void preloadCell(MWWorld::CellStore* cell, bool preloadSurrounding=false);
-
         public:
 
             Scene (MWRender::RenderingManager& rendering, MWPhysics::PhysicsSystem *physics);
 
             ~Scene();
+
+            void preloadCell(MWWorld::CellStore* cell, bool preloadSurrounding=false);
 
             void unloadCell (CellStoreCollection::iterator iter);
 

commit 9e81ed210949802f08310bcac8bb257eb04408af

Fix crash introduced by commit 2a0c865a2055fcc5f77cd93348d2a1c115e80ff1 (Fixes #3940)

apps/openmw/mwworld/worldimp.cpp
@@ -379,8 +379,11 @@ namespace MWWorld
                 return;
             case ESM::REC_PLAY:
                 mPlayer->readRecord(reader, type);
-                mWorldScene->preloadCell(getPlayerPtr().getCell(), true);
-                mWorldScene->preloadTerrain(getPlayerPtr().getRefData().getPosition().asVec3());
+                if (getPlayerPtr().isInCell())
+                {
+                    mWorldScene->preloadCell(getPlayerPtr().getCell(), true);
+                    mWorldScene->preloadTerrain(getPlayerPtr().getRefData().getPosition().asVec3());
+                }
                 break;
             default:
                 if (!mStore.readRecord (reader, type) &&