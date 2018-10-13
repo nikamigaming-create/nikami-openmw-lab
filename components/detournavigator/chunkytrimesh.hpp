elsid/openmw

Add enums for area type and flags

components/detournavigator/chunkytrimesh.hpp
@@ -1,6 +1,8 @@
 #ifndef OPENMW_COMPONENTS_DETOURNAVIGATOR_CHUNKYTRIMESH_H
 #define OPENMW_COMPONENTS_DETOURNAVIGATOR_CHUNKYTRIMESH_H
 
+#include "area_type.hpp"
+
 #include <osg/Vec2f>
 
 #include <array>
@@ -24,7 +26,7 @@ namespace DetourNavigator
     struct Chunk
     {
         const int* const mIndices;
-        const unsigned char* const mFlags;
+        const AreaType* const mAreaTypes;
         const std::size_t mSize;
     };
 
@@ -42,7 +44,7 @@ namespace DetourNavigator
         /// Creates partitioned triangle mesh (AABB tree),
         /// where each node contains at max trisPerChunk triangles.
         ChunkyTriMesh(const std::vector<float>& verts, const std::vector<int>& tris,
-                      const std::vector<unsigned char>& flags, const std::size_t trisPerChunk);
+                      const std::vector<AreaType>& flags, const std::size_t trisPerChunk);
 
         ChunkyTriMesh(const ChunkyTriMesh&) = delete;
         ChunkyTriMesh& operator=(const ChunkyTriMesh&) = delete;
@@ -76,7 +78,7 @@ namespace DetourNavigator
             const auto& node = mNodes[chunkId];
             return Chunk {
                 mIndices.data() + node.mOffset * 3,
-                mFlags.data() + node.mOffset,
+                mAreaTypes.data() + node.mOffset,
                 node.mSize
             };
         }
@@ -89,7 +91,7 @@ namespace DetourNavigator
     private:
         std::vector<ChunkyTriMeshNode> mNodes;
         std::vector<int> mIndices;
-        std::vector<unsigned char> mFlags;
+        std::vector<AreaType> mAreaTypes;
         std::size_t mMaxTrisPerChunk;
     };
 }

components/detournavigator/recastmesh.hpp
@@ -1,6 +1,8 @@
 #ifndef OPENMW_COMPONENTS_DETOURNAVIGATOR_RECASTMESH_H
 #define OPENMW_COMPONENTS_DETOURNAVIGATOR_RECASTMESH_H
 
+#include "area_type.hpp"
+
 #include <memory>
 #include <vector>
 
@@ -15,7 +17,7 @@ namespace DetourNavigator
     {
     public:
         RecastMesh(std::vector<int> indices, std::vector<float> vertices,
-                   std::vector<unsigned char> flags, const Settings& settings);
+                   std::vector<AreaType> areaTypes, const Settings& settings);
 
         const std::vector<int>& getIndices() const
         {
@@ -27,9 +29,9 @@ namespace DetourNavigator
             return mVertices;
         }
 
-        const std::vector<unsigned char>& getFlags() const
+        const std::vector<AreaType>& getAreaTypes() const
         {
-            return mFlags;
+            return mAreaTypes;
         }
 
         std::size_t getVerticesCount() const
@@ -60,7 +62,7 @@ namespace DetourNavigator
     private:
         std::vector<int> mIndices;
         std::vector<float> mVertices;
-        std::vector<unsigned char> mFlags;
+        std::vector<AreaType> mAreaTypes;
         const Settings& mSettings;
         std::unique_ptr<ChunkyTriMesh> mChunkyTriMesh;
         osg::Vec3f mBoundsMin;

nikamigaming-create/nikami-openmw-lab

components/detournavigator/chunkytrimesh.hpp
@@ -1,6 +1,8 @@
 #ifndef OPENMW_COMPONENTS_DETOURNAVIGATOR_CHUNKYTRIMESH_H
 #define OPENMW_COMPONENTS_DETOURNAVIGATOR_CHUNKYTRIMESH_H
 
+#include "areatype.hpp"
+
 #include <osg/Vec2f>
 
 #include <array>
@@ -24,7 +26,7 @@ namespace DetourNavigator
     struct Chunk
     {
         const int* const mIndices;
-        const unsigned char* const mFlags;
+        const AreaType* const mAreaTypes;
         const std::size_t mSize;
     };
 
@@ -42,7 +44,7 @@ namespace DetourNavigator
         /// Creates partitioned triangle mesh (AABB tree),
         /// where each node contains at max trisPerChunk triangles.
         ChunkyTriMesh(const std::vector<float>& verts, const std::vector<int>& tris,
-                      const std::vector<unsigned char>& flags, const std::size_t trisPerChunk);
+                      const std::vector<AreaType>& flags, const std::size_t trisPerChunk);
 
         ChunkyTriMesh(const ChunkyTriMesh&) = delete;
         ChunkyTriMesh& operator=(const ChunkyTriMesh&) = delete;
@@ -76,7 +78,7 @@ namespace DetourNavigator
             const auto& node = mNodes[chunkId];
             return Chunk {
                 mIndices.data() + node.mOffset * 3,
-                mFlags.data() + node.mOffset,
+                mAreaTypes.data() + node.mOffset,
                 node.mSize
             };
         }
@@ -89,7 +91,7 @@ namespace DetourNavigator
     private:
         std::vector<ChunkyTriMeshNode> mNodes;
         std::vector<int> mIndices;
-        std::vector<unsigned char> mFlags;
+        std::vector<AreaType> mAreaTypes;
         std::size_t mMaxTrisPerChunk;
     };
 }

components/detournavigator/recastmesh.hpp
@@ -1,6 +1,7 @@
 #ifndef OPENMW_COMPONENTS_DETOURNAVIGATOR_RECASTMESH_H
 #define OPENMW_COMPONENTS_DETOURNAVIGATOR_RECASTMESH_H
 
+#include "areatype.hpp"
 #include "chunkytrimesh.hpp"
 
 #include <memory>
@@ -16,7 +17,7 @@ namespace DetourNavigator
     {
     public:
         RecastMesh(std::vector<int> indices, std::vector<float> vertices,
-                   std::vector<unsigned char> flags, const Settings& settings);
+                   std::vector<AreaType> areaTypes, const Settings& settings);
 
         const std::vector<int>& getIndices() const
         {
@@ -28,9 +29,9 @@ namespace DetourNavigator
             return mVertices;
         }
 
-        const std::vector<unsigned char>& getFlags() const
+        const std::vector<AreaType>& getAreaTypes() const
         {
-            return mFlags;
+            return mAreaTypes;
         }
 
         std::size_t getVerticesCount() const
@@ -61,7 +62,7 @@ namespace DetourNavigator
     private:
         std::vector<int> mIndices;
         std::vector<float> mVertices;
-        std::vector<unsigned char> mFlags;
+        std::vector<AreaType> mAreaTypes;
         ChunkyTriMesh mChunkyTriMesh;
         osg::Vec3f mBoundsMin;
         osg::Vec3f mBoundsMax;