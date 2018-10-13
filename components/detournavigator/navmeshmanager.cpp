elsid/openmw

Do not use NavMesh to find changed tiles

components/detournavigator/navmeshmanager.cpp
@@ -1,6 +1,7 @@
 #include "navmeshmanager.hpp"
 #include "debug.hpp"
 #include "exceptions.hpp"
+#include "gettilespositions.hpp"
 #include "makenavmesh.hpp"
 #include "settings.hpp"
 #include "sharednavmesh.hpp"
@@ -17,8 +18,7 @@ namespace DetourNavigator
         : mSettings(settings)
         , mRecastMeshManager(settings)
         , mAsyncNavMeshUpdater(settings)
-    {
-    }
+    {}
 
     bool NavMeshManager::addObject(std::size_t id, const btCollisionShape& shape, const btTransform& transform)
     {
@@ -64,12 +64,10 @@ namespace DetourNavigator
         const auto changedTiles = mChangedTiles.find(agentHalfExtents);
         if (changedTiles != mChangedTiles.end())
         {
-            TilePosition playerTile;
             playerPosition *= mSettings.mRecastScaleFactor;
             std::swap(playerPosition.y(), playerPosition.z());
-            cached->mValue.raw()->calcTileLoc(playerPosition.ptr(), &playerTile.x(), &playerTile.y());
-            mAsyncNavMeshUpdater.post(agentHalfExtents, mRecastMeshManager.getMesh(), cached, playerTile,
-                                      changedTiles->second);
+            mAsyncNavMeshUpdater.post(agentHalfExtents, mRecastMeshManager.getMesh(), cached,
+                                      getTilePosition(playerPosition, mSettings), changedTiles->second);
             mChangedTiles.erase(changedTiles);
             log("cache update posted for agent=", agentHalfExtents);
         }
@@ -92,39 +90,11 @@ namespace DetourNavigator
 
     void NavMeshManager::addChangedTiles(const btCollisionShape& shape, const btTransform& transform)
     {
-        btVector3 aabbMin;
-        btVector3 aabbMax;
-        shape.getAabb(transform, aabbMin, aabbMax);
-        osg::Vec3f min(aabbMin.x(), aabbMin.z(), aabbMin.y());
-        osg::Vec3f max(aabbMax.x(), aabbMax.z(), aabbMax.y());
-        min *= mSettings.mRecastScaleFactor;
-        max *= mSettings.mRecastScaleFactor;
-
-        for (auto& v : mCache)
-        {
-            if (const auto& item = v.second)
-            {
-                auto& changedTiles = mChangedTiles[v.first];
-
-                int minTileX;
-                int minTileY;
-                item->mValue.raw()->calcTileLoc(min.ptr(), &minTileX, &minTileY);
-
-                int maxTileX;
-                int maxTileY;
-                item->mValue.raw()->calcTileLoc(max.ptr(), &maxTileX, &maxTileY);
-
-                if (minTileX > maxTileX)
-                    std::swap(minTileX, maxTileX);
-
-                if (minTileY > maxTileY)
-                    std::swap(minTileY, maxTileY);
-
-                for (int tileX = minTileX; tileX <= maxTileX; ++tileX)
-                    for (int tileY = minTileY; tileY <= maxTileY; ++tileY)
-                        changedTiles.insert(TilePosition {tileX, tileY});
-            }
-        }
+        getTilesPositions(shape, transform, mSettings, [&] (const TilePosition& v) {
+            for (const auto& cached : mCache)
+                if (cached.second)
+                    mChangedTiles[cached.first].insert(v);
+        });
     }
 
     std::shared_ptr<NavMeshCacheItem> NavMeshManager::getCached(const osg::Vec3f& agentHalfExtents) const

nikamigaming-create/nikami-openmw-lab

components/detournavigator/navmeshmanager.cpp
@@ -1,6 +1,7 @@
 #include "navmeshmanager.hpp"
 #include "debug.hpp"
 #include "exceptions.hpp"
+#include "gettilespositions.hpp"
 #include "makenavmesh.hpp"
 #include "settings.hpp"
 #include "sharednavmesh.hpp"
@@ -63,12 +64,10 @@ namespace DetourNavigator
         const auto changedTiles = mChangedTiles.find(agentHalfExtents);
         if (changedTiles != mChangedTiles.end())
         {
-            TilePosition playerTile;
             playerPosition *= mSettings.mRecastScaleFactor;
             std::swap(playerPosition.y(), playerPosition.z());
-            cached->mValue.raw()->calcTileLoc(playerPosition.ptr(), &playerTile.x(), &playerTile.y());
-            mAsyncNavMeshUpdater.post(agentHalfExtents, mRecastMeshManager.getMesh(), cached, playerTile,
-                                      changedTiles->second);
+            mAsyncNavMeshUpdater.post(agentHalfExtents, mRecastMeshManager.getMesh(), cached,
+                                      getTilePosition(mSettings, playerPosition), changedTiles->second);
             mChangedTiles.erase(changedTiles);
             log("cache update posted for agent=", agentHalfExtents);
         }
@@ -91,39 +90,11 @@ namespace DetourNavigator
 
     void NavMeshManager::addChangedTiles(const btCollisionShape& shape, const btTransform& transform)
     {
-        btVector3 aabbMin;
-        btVector3 aabbMax;
-        shape.getAabb(transform, aabbMin, aabbMax);
-        osg::Vec3f min(aabbMin.x(), aabbMin.z(), aabbMin.y());
-        osg::Vec3f max(aabbMax.x(), aabbMax.z(), aabbMax.y());
-        min *= mSettings.mRecastScaleFactor;
-        max *= mSettings.mRecastScaleFactor;
-
-        for (auto& v : mCache)
-        {
-            if (const auto& item = v.second)
-            {
-                auto& changedTiles = mChangedTiles[v.first];
-
-                int minTileX;
-                int minTileY;
-                item->mValue.raw()->calcTileLoc(min.ptr(), &minTileX, &minTileY);
-
-                int maxTileX;
-                int maxTileY;
-                item->mValue.raw()->calcTileLoc(max.ptr(), &maxTileX, &maxTileY);
-
-                if (minTileX > maxTileX)
-                    std::swap(minTileX, maxTileX);
-
-                if (minTileY > maxTileY)
-                    std::swap(minTileY, maxTileY);
-
-                for (int tileX = minTileX; tileX <= maxTileX; ++tileX)
-                    for (int tileY = minTileY; tileY <= maxTileY; ++tileY)
-                        changedTiles.insert(TilePosition {tileX, tileY});
-            }
-        }
+        getTilesPositions(shape, transform, mSettings, [&] (const TilePosition& v) {
+            for (const auto& cached : mCache)
+                if (cached.second)
+                    mChangedTiles[cached.first].insert(v);
+        });
     }
 
     const std::shared_ptr<NavMeshCacheItem>& NavMeshManager::getCached(const osg::Vec3f& agentHalfExtents) const