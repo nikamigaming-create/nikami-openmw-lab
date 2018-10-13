elsid/openmw

Prioritise NavMesh jobs first to remove and last to add
When player move fast enough, tiles update for specific area square
couldn't catch player move. Tiles to be removed are left in the queue
with lower priority then tiles to be added which are nearest to player.
This can lead to overflow for amount of tiles. So we try to do remove
first. But we detect change type approximately using mixed change type,
because even if we do it precise, change type could change while job
is in queue.

components/detournavigator/asyncnavmeshupdater.cpp
@@ -7,16 +7,22 @@
 
 namespace
 {
+    using DetourNavigator::ChangeType;
     using DetourNavigator::TilePosition;
 
-    int getDistance(const TilePosition& lhs, const TilePosition& rhs)
+    int getManhattanDistance(const TilePosition& lhs, const TilePosition& rhs)
     {
         return std::abs(lhs.x() - rhs.x()) + std::abs(lhs.y() - rhs.y());
     }
 
-    std::pair<int, int> makePriority(const TilePosition& changedTile, const TilePosition& playerTile)
+    std::tuple<ChangeType, int, int> makePriority(const TilePosition& position, const ChangeType changeType,
+                                                    const TilePosition& playerTile)
     {
-        return std::make_pair(getDistance(changedTile, playerTile), getDistance(changedTile, TilePosition {0, 0}));
+        return std::make_tuple(
+            changeType,
+            getManhattanDistance(position, playerTile),
+            getManhattanDistance(position, TilePosition {0, 0})
+        );
     }
 }
 
@@ -58,16 +64,16 @@ namespace DetourNavigator
 
     void AsyncNavMeshUpdater::post(const osg::Vec3f& agentHalfExtents,
         const std::shared_ptr<NavMeshCacheItem>& mNavMeshCacheItem, const TilePosition& playerTile,
-        const std::set<TilePosition>& changedTiles)
+        const std::map<TilePosition, ChangeType>& changedTiles)
     {
         log("post jobs playerTile=", playerTile);
         const std::lock_guard<std::mutex> lock(mMutex);
         mPlayerTile = playerTile;
         for (const auto& changedTile : changedTiles)
         {
-            if (mPushed[agentHalfExtents].insert(changedTile).second)
-                mJobs.push(Job {agentHalfExtents, mNavMeshCacheItem, changedTile,
-                                makePriority(changedTile, playerTile)});
+            if (mPushed[agentHalfExtents].insert(changedTile.first).second)
+                mJobs.push(Job {agentHalfExtents, mNavMeshCacheItem, changedTile.first,
+                                makePriority(changedTile.first, changedTile.second, playerTile)});
         }
         log("posted ", mJobs.size(), " jobs");
         mHasJob.notify_all();

nikamigaming-create/nikami-openmw-lab

components/detournavigator/asyncnavmeshupdater.cpp
@@ -9,16 +9,22 @@
 
 namespace
 {
+    using DetourNavigator::ChangeType;
     using DetourNavigator::TilePosition;
 
-    int getDistance(const TilePosition& lhs, const TilePosition& rhs)
+    int getManhattanDistance(const TilePosition& lhs, const TilePosition& rhs)
     {
         return std::abs(lhs.x() - rhs.x()) + std::abs(lhs.y() - rhs.y());
     }
 
-    std::pair<int, int> makePriority(const TilePosition& changedTile, const TilePosition& playerTile)
+    std::tuple<ChangeType, int, int> makePriority(const TilePosition& position, const ChangeType changeType,
+                                                    const TilePosition& playerTile)
     {
-        return std::make_pair(getDistance(changedTile, playerTile), getDistance(changedTile, TilePosition {0, 0}));
+        return std::make_tuple(
+            changeType,
+            getManhattanDistance(position, playerTile),
+            getManhattanDistance(position, TilePosition {0, 0})
+        );
     }
 }
 
@@ -60,7 +66,7 @@ namespace DetourNavigator
 
     void AsyncNavMeshUpdater::post(const osg::Vec3f& agentHalfExtents,
         const std::shared_ptr<NavMeshCacheItem>& navMeshCacheItem, const TilePosition& playerTile,
-        const std::set<TilePosition>& changedTiles)
+        const std::map<TilePosition, ChangeType>& changedTiles)
     {
         log("post jobs playerTile=", playerTile);
 
@@ -73,9 +79,9 @@ namespace DetourNavigator
 
         for (const auto& changedTile : changedTiles)
         {
-            if (mPushed[agentHalfExtents].insert(changedTile).second)
-                mJobs.push(Job {agentHalfExtents, navMeshCacheItem, changedTile,
-                                makePriority(changedTile, playerTile)});
+            if (mPushed[agentHalfExtents].insert(changedTile.first).second)
+                mJobs.push(Job {agentHalfExtents, navMeshCacheItem, changedTile.first,
+                                makePriority(changedTile.first, changedTile.second, playerTile)});
         }
 
         log("posted ", mJobs.size(), " jobs");