Sisah2/openmw

Write generated navmesh to navmeshdb
Perform all request to db in a single thread to avoid blocking navmesh
generator threads due to slow write operations.

Write to db navmesh for all changes except update as it done for memory cache.

Batch multiple db operations into a single transaction to speed up writing by
not executing fsync after each insert/update query. All reads are performed in
the same transaction so they see uncommited data.

components/detournavigator/settings.cpp
@@ -43,6 +43,7 @@ namespace DetourNavigator
         navigatorSettings.mEnableNavMeshFileNameRevision = ::Settings::Manager::getBool("enable nav mesh file name revision", "Navigator");
         navigatorSettings.mMinUpdateInterval = std::chrono::milliseconds(::Settings::Manager::getInt("min update interval ms", "Navigator"));
         navigatorSettings.mNavMeshVersion = static_cast<std::int64_t>(::Settings::Manager::getInt("nav mesh version", "Navigator"));
+        navigatorSettings.mWriteToNavMeshDb = ::Settings::Manager::getBool("write to navmeshdb", "Navigator");
 
         return navigatorSettings;
     }

nikamigaming-create/nikami-openmw-lab

components/detournavigator/settings.cpp
@@ -63,6 +63,7 @@ namespace DetourNavigator
         result.mMinUpdateInterval = std::chrono::milliseconds(::Settings::Manager::getInt("min update interval ms", "Navigator"));
         result.mNavMeshVersion = ::Settings::Manager::getInt("nav mesh version", "Navigator");
         result.mEnableNavMeshDiskCache = ::Settings::Manager::getBool("enable nav mesh disk cache", "Navigator");
+        result.mWriteToNavMeshDb = ::Settings::Manager::getBool("write to navmeshdb", "Navigator");
 
         return result;
     }