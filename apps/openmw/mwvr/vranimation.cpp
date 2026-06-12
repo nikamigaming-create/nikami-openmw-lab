a24b5b6dc7fb3998e4dc869028eecba5b8fd036e
Nikami Creative <295006744+nikamigaming-create@users.noreply.github.com>
2026-06-12 13:50:56 -0700

    Revert "Mount Fallout VR hands from hand bind matrix"
    This reverts commit c7e3738b64476f72eaf4223e103937111fe53036.

--- apps/openmw/mwvr/vranimation.cpp
+++ apps/openmw/mwvr/vranimation.cpp
@@ -125,6 +125,32 @@ namespace MWVR
         int mRiggedDrawables = 0;
     };
 
+    class FindNamedNodeVisitor : public osg::NodeVisitor
+    {
+    public:
+        explicit FindNamedNodeVisitor(std::string_view name)
+            : osg::NodeVisitor(TRAVERSE_ALL_CHILDREN)
+            , mName(name)
+        {
+        }
+
+        void apply(osg::Node& node) override
+        {
+            if (mFound == nullptr && node.getName() == mName)
+            {
+                mFound = &node;
+                return;
+            }
+
+            traverse(node);
+        }
+
+        osg::ref_ptr<osg::Node> mFound;
+
+    private:
+        std::string mName;
+    };
+
     class StaticizeFalloutVrHandRigVisitor : public osg::NodeVisitor
     {
     public:
@@ -277,11 +303,37 @@ namespace MWVR
         cloned->accept(boundsVisitor);
         const osg::BoundingBox bounds = boundsVisitor.getBoundingBox();
         osg::Matrix handLocal;
-        if (!handLocal.invert(handInBip))
-            handLocal.makeIdentity();
+        handLocal.makeIdentity();
         osg::Vec3f center;
+        osg::Vec3f anchor;
+        std::string anchorSource = "none";
         if (bounds.valid())
+        {
             center = bounds.center();
+            anchor = center;
+            anchorSource = "boundsCenter";
+        }
+
+        FindNamedNodeVisitor handBoneFinder(bone);
+        cloned->accept(handBoneFinder);
+        if (handBoneFinder.mFound != nullptr)
+        {
+            osg::NodePathList paths = handBoneFinder.mFound->getParentalNodePaths(cloned.get());
+            if (paths.empty())
+                paths = handBoneFinder.mFound->getParentalNodePaths();
+            if (!paths.empty())
+            {
+                anchor = osg::computeLocalToWorld(paths.front()).getTrans();
+                anchorSource = "templateBone";
+            }
+        }
+
+        if (bounds.valid() && anchorSource == "boundsCenter")
+        {
+            anchor = osg::Vec3f(center.x(), bounds.yMin(), center.z());
+            anchorSource = "boundsWristSideYMin";
+        }
+        handLocal.setTrans(-anchor);
         const bool leftHand = bone.find(" L ") != std::string_view::npos;
         const bool leftRollFlip = leftHand && Settings::vr().mFalloutLeftHandRollFlip;
         if (leftRollFlip)
@@ -316,8 +368,8 @@ namespace MWVR
                          << handLocal.getTrans().x() << "," << handLocal.getTrans().y() << ","
                          << handLocal.getTrans().z() << ") quat=(" << localRotation.x() << ","
                          << localRotation.y() << "," << localRotation.z() << "," << localRotation.w()
-                         << ") anchorSource=inverseHandInBip sourceCenter=(" << center.x() << "," << center.y()
-                         << "," << center.z()
+                         << ") anchorSource=" << anchorSource << " anchor=(" << anchor.x() << "," << anchor.y()
+                         << "," << anchor.z() << ") sourceCenter=(" << center.x() << "," << center.y() << "," << center.z()
                          << ") wristCalibration=(" << falloutWristCalibration.x() << ","
                          << falloutWristCalibration.y() << "," << falloutWristCalibration.z()
                          << ") meshRotationDeg=(" << osg::RadiansToDegrees(falloutMeshRotation.x()) << ","
@@ -332,8 +384,7 @@ namespace MWVR
                              << bounds.xMin() << "," << bounds.yMin() << "," << bounds.zMin()
                              << ") max=(" << bounds.xMax() << "," << bounds.yMax() << "," << bounds.zMax()
                              << ") center=(" << center.x() << "," << center.y() << "," << center.z()
-                             << ") centerDistance=" << center.length() << " inverseHandInBipDistance="
-                             << handInBip.getTrans().length();
+                             << ") centerDistance=" << center.length() << " anchorDistance=" << anchor.length();
         }
 
         attachNode->addChild(transform);
