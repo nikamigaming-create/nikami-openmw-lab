b276f42a1cd3a552a42236722692d640fefdab9b
Nikami Creative <295006744+nikamigaming-create@users.noreply.github.com>
2026-07-19 06:15:42 -0700

    Activate native FNV HUD policy after load

--- apps/openmw/mwstate/statemanagerimp.cpp
+++ apps/openmw/mwstate/statemanagerimp.cpp
@@ -596,6 +596,7 @@ void MWState::StateManager::loadGame(const Character* character, const std::file
         mutableWorld.updateProjectilesCasters();
         MWBase::Environment::get().getWorldScene()->markCellAsUnchanged();
         MWBase::Environment::get().getLuaManager()->gameLoaded();
+        MWBase::Environment::get().getWindowManager()->setLegacyHudSuppressed(true);
         mNativeFalloutSaveLoaded = true;
         Log(Debug::Info) << "Native FNV save owns camera mode=" << static_cast<int>(camera->getMode())
                          << " pitch=" << camera->getPitch() << " yaw=" << camera->getYaw()
